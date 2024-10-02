a5def55229b7b4e85f1fd9be61ed4f3b90a86db7
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2024-10-02 13:23:35 +0300

    Add state and effect

---
+++ src/components/description/Description.jsx
@@ -0,0 +1,12 @@
+import css from './Description.module.css'
+
+function Description() {
+    return (
+        <>
+            <h2 className={css.title}>Sip Happens Café</h2>
+            <p>Please leave your feedback about our service by selecting one of the options below.</p>
+        </>
+    )
+}
+
+export default Description
